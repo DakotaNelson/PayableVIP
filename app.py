b3fbe74cc0b0853d5a07c8629d73090a9762087c
Arjun Bhatnagar <me@arjunb.com>
2015-10-24 22:00:52 -0700

    website

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from parse_rest.connection import register
+from parse_rest.user import User
+
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -12,6 +14,18 @@ register(app.config['PARSE_APP_ID'],
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/test/')
+def test():
+    u = User.login("arjun", "password")
+
+    return render_template('test.html', u=u.username)
+
+@app.route('/questions/')
+def questions():
+    u = User.login("arjun", "password")
+
+    return render_template('questions.html', u=u.username)
 
 @app.route('/dashboard')
 def dashboard():
