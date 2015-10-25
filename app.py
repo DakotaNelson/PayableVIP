f5b4f844e9c50166c51344d8af6005fbca9f3023
Dakota Nelson <Dakota.Nelson@students.olin.edu>
2015-10-24 23:18:58 -0400

    can now conduct ACH transfers and mostly add new accounts for users

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from parse_rest.connection import register
+import json
+
+from utils import achCharge
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -17,6 +20,38 @@ def index():
 def dashboard():
     customers = [{"fname":"Bob"}, {"fname":"Joe"}]
     return render_template('dashboard.html', inStore=customers)
+
+@app.route('/api/add-account', methods=['POST'])
+def addAccount():
+    req = request.get_json()
+
+    if req is None:
+        return json.dumps({"status": "fail",
+                "reason": "Must include JSON."})
+
+    if not 'username' in req:
+        return json.dumps({"status": "fail",
+                "reason": "Must include username."})
+
+    if not 'routingNo' in req or not 'acctNo' in req:
+        return json.dumps({"status": "fail",
+                "reason": "Must include routing and account number."})
+
+    # validate the account with a $1 charge
+    res = achCharge(1, req['routingNo'], req['acctNo'])
+
+    if res['CmdStatus'] != 'Approved':
+        return json.dumps({"status": "fail",
+                "reason": "Account invalid."})
+
+    """
+    {u'Authorize': u'5.00', u'Purchase': u'5.00', u'AcctNo': u'XXXXXXXXXXXXXX67', u'ResponseOrigin': u'Processor', u'CmdStatus': u'Approved', u'AuthCode': u'272-172', u'TranCode': u'Authorize', u'UserTraceData': u'', u'TextResponse': u'Approved', u'InvoiceNo': u'111020141280', u'CardType': u'ACH', u'DSIXReturnCode': u'000000', u'MerchantID': u'6013521114', u'OperatorID': u'TEST'}
+    """
+
+    # store the routing and account number in the db
+    # make sure to credit the user for the dollar we took earlier
+
+    return json.dumps({"status":"success"})
 
 if __name__ == '__main__':
     app.run()
---
+++ utils.py
@@ -0,0 +1,42 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+def achCharge(amount, routingNo, acctNo):
+    """
+    {
+    "InvoiceNo":"111020141127",
+    "InvoiceNo":"111020141280",
+    "RoutingNo":"490000018",
+    "AcctNo":"2441234567134567",
+    "SequenceNo":"9999",
+    "AcctType":"Checking",
+    "ACHMethod":"VerificationOnly",
+    "OperatorID":"TEST",
+    "Purchase":"1.50",
+    }
+
+    curl -k -v -X POST -H "Content-Type:application/json" -H "Authorization: Basic NjAxMzUyMTExNDp4eXo=" -d "@ach.txt" -o output.txt https://w1.mercurycert.net/paymentsapi/ACH/Authorize
+
+    """
+
+    params = {
+        "InvoiceNo":"111020141127",
+        "InvoiceNo":"111020141280",
+        "RoutingNo": str(routingNo),
+        "AcctNo": str(acctNo),
+        "SequenceNo":"9999",
+        "AcctType":"Checking",
+        "ACHMethod":"VerificationOnly",
+        "OperatorID":"TEST",
+        "Purchase": "{0:.2f}".format(amount)
+    }
+
+    r = requests.post('https://w1.mercurycert.net/paymentsapi/ACH/Authorize',
+                      json = params,
+                      auth = HTTPBasicAuth('6013521114','xyz'))
+
+    return r.json()
+
+def getInvoiceNo():
+    # must generate a unique (incrementing) integer
+    pass
