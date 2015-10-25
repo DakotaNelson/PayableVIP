01f447fe1c202978b9a9ce18c333744cb63fa25b
Arjun Bhatnagar <me@arjunb.com>
2015-10-25 10:19:48 -0700

    merged.. for realz.

--- static/js/button.js
+++ static/js/button.js
@@ -2,6 +2,7 @@ $(function() {
     $('button').click(function() {
         var user = 'arjun';
         var pass = 'password';
+        $(".result").text("Processing...");
         $.ajax({
             url: '/api/make-payment/',
             data: JSON.stringify({"username": "arjun",
@@ -12,8 +13,12 @@ $(function() {
             dataType: 'json',
             success: function(response) {
                 console.log(response);
+                $(".result").text("Success!");
+                $(".cost").text("$0");
+                $(".subcost").text("0");
             },
             error: function(error) {
+                $(".result").text("Could not make payment. :(");
                 console.log(error);
             }
         });
